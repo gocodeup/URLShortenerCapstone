5242fddb079b77437552bf765abb7d67014dd428
Jeremy Price <jeremy@codeup.com>
2022-04-26 15:39:14 -0500

    initial testing to insert the info into the database

--- storage.py
+++ storage.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 
 import datetime
+import psycopg  # use PostgreSQL
 '''Storage layer for our URL shortener project.  Translates get/set requests into the backend storage interface.'''
+
+CONNECTION_STRING = 'dbname=url_shortener user=url_shortener password=Passw0rd'
 
 def get_info(short_code: str):
     '''
@@ -12,14 +15,19 @@ def get_info(short_code: str):
     '''
     return None
 
-def store_url(url: str, timestamp: datetime.datetime):
+def store_url(url: str, timestamp: datetime.datetime, ip_address: str, short_code: str):
     '''
     Store the given url with the extra metadata into the storage backend.
     Returns:
-      None if there was an error storing the information
-      new short_code on successful storage
+      False if there was an error storing the information
+      True on successful storage
     '''
-    return None
+    with psycopg.connect(CONNECTION_STRING) as db_connection:
+        with db_connection.cursor() as cur:
+            cur.execute('INSERT INTO urls (url, shortcode, created, creator) VALUES (%s, %s, %s, %s);',
+                        (url, short_code, timestamp, ip_address))
+            db_connection.commit()
+    return True
 
 def get_short_code(url: str):
     '''
@@ -29,6 +37,3 @@ def get_short_code(url: str):
       short_code if the url is in the storage engine
     '''
     return None
-
-def initialize_db_connection():
-    pass
--- urlshortener.py
+++ urlshortener.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 
-'''Simple Flask server that shortens URLs into 8 characters or less.'''
+'''Simple Flask server that shortens URLs into 10 characters.'''
 
 from flask import Flask
 import logging
+import storage
+import datetime
+import hashlib
 
 app_log = logging.getLogger(__name__)
 app = Flask(__name__)
@@ -22,3 +25,18 @@ def get_url(short_code):
 def get_info(short_code: str):
     '''Get the info (created time, associated url, etc.) for the given short code'''
     return
+
+def generate_short_code(url):
+    '''Generate a short code given the url'''
+    length = 10
+    return hashlib.sha256(url.encode()).hexdigest()[:length]
+
+def test_storage():
+    url = 'https://codeup.com'
+    timestamp = datetime.datetime.now(datetime.timezone.utc)
+    ip_address = '127.0.0.1'
+    short_code = generate_short_code(url)
+    storage.store_url(url=url, timestamp=timestamp, ip_address=ip_address, short_code=short_code)
+
+if __name__ == '__main__':
+    test_storage()
