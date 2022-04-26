1078b791ac31d1c00623e5e363cb47aa2a55725f
Jeremy Price <jeremy@codeup.com>
2022-04-26 16:11:37 -0500

    added the code for searching for a code/url

--- storage.py
+++ storage.py
@@ -1,26 +1,40 @@
 #!/usr/bin/env python3
 
 import datetime
+from xmlrpc.client import boolean
 import psycopg  # use PostgreSQL
+from typing import Optional
 '''Storage layer for our URL shortener project.  Translates get/set requests into the backend storage interface.'''
 
 CONNECTION_STRING = 'dbname=url_shortener user=url_shortener password=Passw0rd'
 
-def get_info(short_code: str):
+def get_info(short_code: Optional[str]=None, url: Optional[str]=None) -> Optional[dict]:
     '''
-    Get the info for the requested short_code.
+    Get the info for the requested short_code or url.
     Returns: 
-      None if short_code doesn't exist
-      info dictionary associated with short_code if short_code exists
+        None if short_code doesn't exist
+        info dictionary associated with short_code if short_code exists
     '''
-    return None
-
-def store_url(url: str, timestamp: datetime.datetime, ip_address: str, short_code: str):
+    with psycopg.connect(CONNECTION_STRING) as db_connection:
+        with db_connection.cursor() as cur:
+            if short_code:
+                param = 'shortcode'
+                val = short_code
+            elif url:
+                param = 'url'
+                val = url
+            cur.execute(f'SELECT * FROM urls WHERE {param} = %s', (val, ))
+            record = cur.fetchone()
+            if record is None:
+                return None
+            else:
+                return {'url': record[1], 'short_code': record[2], 'timestamp': record[3], 'ip_address': str(record[4])}
+def store_url(url: str, timestamp: datetime.datetime, ip_address: str, short_code: str) -> boolean:
     '''
     Store the given url with the extra metadata into the storage backend.
     Returns:
-      False if there was an error storing the information
-      True on successful storage
+        False if there was an error storing the information
+        True on successful storage
     '''
     with psycopg.connect(CONNECTION_STRING) as db_connection:
         with db_connection.cursor() as cur:
@@ -28,12 +42,3 @@ def store_url(url: str, timestamp: datetime.datetime, ip_address: str, short_cod
                         (url, short_code, timestamp, ip_address))
             db_connection.commit()
     return True
-
-def get_short_code(url: str):
-    '''
-    Get the short_code associated with the given URL.
-    Returns:
-      None if no short_code exists for the URL
-      short_code if the url is in the storage engine
-    '''
-    return None
--- urlshortener.py
+++ urlshortener.py
@@ -31,12 +31,23 @@ def generate_short_code(url):
     length = 10
     return hashlib.sha256(url.encode()).hexdigest()[:length]
 
-def test_storage():
+def test_store_url():
     url = 'https://codeup.com'
     timestamp = datetime.datetime.now(datetime.timezone.utc)
     ip_address = '127.0.0.1'
     short_code = generate_short_code(url)
     storage.store_url(url=url, timestamp=timestamp, ip_address=ip_address, short_code=short_code)
 
+def test_get_info():
+    # this one should exist
+    short_code = generate_short_code('https://codeup.com')
+    print(storage.get_info(short_code=short_code))
+    # this one won't exist
+    print(storage.get_info(short_code='-'*10))
+    # pull based on url
+    print(storage.get_info(url='https://codeup.com'))
+    # this one won't exist
+    print(storage.get_info(url='-'*10))
+
 if __name__ == '__main__':
-    test_storage()
+    test_get_info()
